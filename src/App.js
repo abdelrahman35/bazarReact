3248a0f6524e4f0d2fc5aa23d9b739bf330f9bf2
lameskhaled <91064862+lameskhaled@users.noreply.github.com>
2022-07-27 14:17:05 +0200

    fix conflict

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import Thanks from "./pages/ThanksForm/Thanks";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/login" index element={<LoginPage />} />
           <Route path="/product-details" iNdex element={<ProductDetails />} />
           <Route path="/signup" index element={<SignUpPage />} />
+          <Route path="/thanks" index element={<Thanks />} />
           <Route path="/products" element={<ProudctPage />} />
           <Route path="/aboutus" index element={<AboutUsPage />} />
           <Route path="/forgetPassword" index element={<ForgetPassword />} />
--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import styles from "./ProductDetails.module.css";
+import image1 from "../../assets/images/Productimage.png";
 
 const ProductDetails = () => {
-  return <></>;
+  return (
+    <>
+      <section>
+        <div className=" container d-flex">
+          <div className="row justify-content-center justify-content-lg-between text-center text-md-start align-items-center">
+            <div className="col-12 col-md-7">
+              <div className={styles.Last}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis.
+                Class aptent taciti sociosqu ad litora torquent per conubia
+                nostra, per inceptos himenaeos. Curabitur tempus urna at turpis
+                condimentutum lobortis. Ut commodo efficitur neque. Ut diam
+                quam, semper iaculis condimentum ac, vestibulum eu nisl.Lorem
+                ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit interdum, ac aliquet odio mattis.
+                Class aptent taciti sociosqu ad litora torquent per conubia
+                nostra, per inceptos himenaeos. Curabitur tempus urna at turpis
+                condimentum lobortis. Ut commodo efficitur neque. Ut diam quam,
+                semper iaculis condimentum ac, vestibulum eu nisl.
+              </div>
+            </div>
+            <div className="col-12 col-md-4">
+              <div>
+                <img src={image1} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProductDetails;
