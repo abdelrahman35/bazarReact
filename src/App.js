eaf075bc319e4b5f6b958fb0905bf3c81fcfac55
lameskhaled <91064862+lameskhaled@users.noreply.github.com>
2022-07-27 14:05:21 +0200

    footer , about us page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
 function App() {
   return (
     <>
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" index element={<Home />} />
           <Route path="/login" index element={<LoginPage />} />
+          <Route path="/product-details" iNdex element={<ProductDetails />} />
           <Route path="/signup" index element={<SignUpPage />} />
           <Route path="/products" element={<ProudctPage />} />
           <Route path="/aboutus" index element={<AboutUsPage />} />
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,93 +5,94 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <div className="">
-        <footer
-          className={`row  text-center text-lg-start fixed-bottom ${styles.Footerstyle}`}
-        >
-          <div className="container p-3">
-            <div className={`row d-flex  ${styles.CenterContainer}`}>
-              <div className="col-lg-3 col-md-6 mb-4 mb-md-0 justify-content-center">
-                <h3 className="text-uppercase ">BAZAAR</h3>
-                <ul className={`list-unstyled mb-0 ${styles.TextColor}`}>
-                  <li>
-                    There are many variations of passages of Lorem Ipsum
-                    available,but the majority have suffered alteration in some
-                    form,by injected humour,or randomised words which don't look
-                    even slightly believable.
+      <footer
+        className={`container-fluid m-0 p-0  text-center text-lg-start  ${styles.Footerstyle}`}
+      >
+        <div className="container p-3">
+          <div className={`row d-flex  ${styles.CenterContainer}`}>
+            <div className="col-12 col-md-4  mb-4 mb-md-0 justify-content-center">
+              <h3 className="text-uppercase ">BAZAAR</h3>
+              <ul className={`list-unstyled mb-0 ${styles.TextColor}`}>
+                <li className={styles.textsize}>
+                  There are many variations of passages of Lorem Ipsum
+                  available,but the majority have suffered alteration in some
+                  form,by injected humour,or randomised words which don't look
+                  even slightly believable.
+                </li>
+                <div className="d-flex mt-3 justify-content-center justify-content-md-start">
+                  <li className="me-2 ">
+                    <Link to="#!" className={styles.listunstyled1}>
+                      <i className="fa-brands fa-facebook"></i>
+                    </Link>
                   </li>
-                  <div className="d-flex mt-3 ">
-                    <li className="me-2 ">
-                      <Link to="#!" className={styles.listunstyled}>
-                        <i className="fa-brands fa-facebook"></i>
-                      </Link>
-                    </li>
-                    <li className="me-2">
-                      <Link to="#!" className={styles.listunstyled}>
-                        <i className="fa-brands fa-twitter"></i>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="#!" className={styles.listunstyled}>
-                        <i className="fa-brands fa-instagram"></i>
-                      </Link>
-                    </li>
-                  </div>
-                </ul>
-              </div>
-              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 className={`${styles.titles}`}>Shop With Us</h5>
+                  <li className="me-2">
+                    <Link to="#!" className={styles.listunstyled1}>
+                      <i className="fa-brands fa-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#!" className={styles.listunstyled1}>
+                      <i className="fa-brands fa-instagram"></i>
+                    </Link>
+                  </li>
+                </div>
+              </ul>
+            </div>
+            <div className="col-12 col-md-4  mb-4 mb-md-0">
+              <h5 className={`${styles.titles}`}>Shop With Us</h5>
 
-                <ul className={styles.listunstyled}>
-                  <li>
-                    <Link to="#!" className={` mt-5 ${styles.listunstyled}`}>
-                      Products
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#!" className={styles.listunstyled}>
-                      Cart
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#!" className={styles.listunstyled}>
-                      Favorite
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#!" className={styles.listunstyled}>
-                      Orders
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 className={` ${styles.titles}`}>Contact Us</h5>
+              <ul className="list-unstyled mb-0 mt-3">
+                <li className="mt4">
+                  <Link to="#!" className={` mt-2 ${styles.listunstyled}`}>
+                    Products
+                  </Link>
+                </li>
+                <li className="mt4">
+                  <Link to="#!" className={styles.listunstyled}>
+                    Cart
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#!" className={styles.listunstyled}>
+                    Favorite
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#!" className={styles.listunstyled}>
+                    Orders
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-12 col-md-4  mb-4 mb-md-0 ">
+              <h5 className={` ${styles.titles}`}>Contact Us</h5>
 
-                <ul className="list-unstyled mb-0">
-                  <li>
-                    <Link to="#!" className={styles.listunstyled}>
-                      info@Bazaar.com
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#!" className={styles.listunstyled}>
-                      +20 01155616679
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#!" className={styles.listunstyled}>
-                      772 Giza , Egypt
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <Link to="#!" className={styles.listunstyled}>
+                    <i class="fa-solid fa-envelopes-bulk me-1"></i>
+                    info@Bazaar.com
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#!" className={styles.listunstyled}>
+                    <i class="fa-solid fa-square-phone me-1"></i> +20
+                    01155616679
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#!" className={styles.listunstyled}>
+                    <i class="fa-solid fa-location-dot me-1"></i>
+                    772 Giza , Egypt
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
+        </div>
 
-          <div className="text-center p-3">© 2020 Copyright:</div>
-        </footer>
-      </div>
+        <div className="text-center p-3">© 2020 Copyright:</div>
+      </footer>
     </>
   );
 };
