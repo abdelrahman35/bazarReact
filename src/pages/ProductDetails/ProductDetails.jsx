81bd848c0917b515741c899bb49ad19e44cab5eb
ahmedibrahim287 <ahmedibrahim287@gmail.com>
2022-08-02 11:33:00 +0200

    product-details redesign

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -6,6 +6,8 @@ import image1 from "../../assets/images/Productimage1.png";
 import { getProductById } from "../../store/actions/productActions";
 import Loading from "../../components/Loading/Loading";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import tryImg from "../../assets/images/Productimage1.png";
+
 const ProductDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -28,11 +30,93 @@ const ProductDetails = () => {
         </div>
       ) : product ? (
         <section>
-          <div className={`container d-flex `}>
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-md-6">
+                <div className="">
+                  <img
+                    className="w-100"
+                    src={`https://bazaarshop.s3.eu-west-3.amazonaws.com${product?.product?.image}`}
+                    alt={product?.product?.name}
+                  />
+                </div>
+              </div>
+              <div className="col-12 col-md-6">
+                <h1 className={styles.title}>{product?.product?.name}</h1>
+                <h3> EGP {product?.product?.price}</h3>
+                <h3> {product?.product?.rating} review</h3>
+                <hr></hr>
+                <h3>model year : {product?.product?.modelYear} </h3>
+                <h3>About this item : {product?.product?.description} </h3>
+                <div className="d-flex">
+                  <select
+                    class="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+
+                  <button className={`${styles.btnWarningg}`}>
+                    <i className="fa-solid fa-cart-plus"></i>
+                    Add to cart
+                  </button>
+                  <div>
+                    <i className="fa-solid fa-square-heart"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <h2>Reviews</h2>
+              <hr></hr>
+              <div className="row">
+                <div className="col-12 col-md-4">
+                  <div
+                    className={`${styles.overallRating} d-flex justify-content-center align-items-center flex-column mt-3`}
+                  >
+                    <h3>Overall Rating</h3>
+                    <h3> {product?.product?.rating}</h3>
+                  </div>
+                </div>
+                <div className="col-12 col-md-4">
+                  <h2> How do I review this product? </h2>
+                  If you recently purchased this product from noon, you can go
+                  to your Orders page and click on the Submit Review button
+                </div>
+                <div className="col-12 col-md-4">
+                  <h2> Where do the reviews come from?</h2> Our reviews are from
+                  noon customers who purchased the product and submitted a
+                  review
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <h2>Customer Reviews</h2>
+              <div className="ms-4">
+                <div className="row">
+                  <h3>Reviewer Name is here</h3>
+                  <h3>Reviewer rate is here</h3>
+                  <h3>date of reviewer</h3>
+                  <p>
+                    The battery is not as indicated it does not stay till 14
+                    days as specified but rather 2 days max but the band overall
+                    is great
+                  </p>
+                  <hr></hr>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      ) : /* <section>
+          <div className={`container-fluid d-flex `}>
             <div
               className={`row justify-content-center justify-content-lg-between ${styles.Content}`}
             >
-              <div className="col-6 col-md-4">
+              <div className="col-12 col-md-6 col-md-4">
                 <div className={styles.imgcontent}>
                   <img
                     src={`${product?.product?.image}`}
@@ -40,11 +124,10 @@ const ProductDetails = () => {
                   />
                 </div>
               </div>
-              <div className="col-6 col-md-4">
+              <div className="col-12 col-md-6 col-md-4">
                 <div className={styles.rightcont}>
                   <h1 className={styles.title}>{product?.product?.name}</h1>
                   <div className={styles.price}>
-                    {" "}
                     EGP {product?.product?.price}
                   </div>
                   <div className={`d-flex ${styles.review} justify-content`}>
@@ -104,8 +187,9 @@ const ProductDetails = () => {
               </div>
             </div>
           </div>
-        </section>
-      ) : productError ? (
+        </section> */
+
+      productError ? (
         <ErrorMessage />
       ) : null}
     </>
