21c667157d9b122b1c8f816bd0fa3ec9453a46d4
abdelrahman <abdelrahman.tarek.omar@gmail.com>
2022-08-12 11:51:18 +0200

    fix add  to cart bugs

--- src/store/reducers/cartReducers.jsx
+++ src/store/reducers/cartReducers.jsx
@@ -1,32 +1,28 @@
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case "ADD_TO_CART_SUCCESS":
+      // product data from action
       const newProduct = action.payload;
-      console.log(state);
-      const isExist = state.cartItems.find(
-        (productFromCart) =>
-          productFromCart.productId.toString() ===
-          newProduct.productId.toString()
+      //check if product is in the cart or not
+      const inCart = state.cartItems.find((item) =>
+        item.productId === newProduct.productId ? true : false
       );
-      console.log(isExist);
-      if (isExist) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map((productFromCart) => {
-            if (productFromCart.productId === isExist.productId) {
-              productFromCart.qty = productFromCart.qty + newProduct.qty;
-              return productFromCart;
-            } else {
-              return newProduct;
-            }
-          }),
-        };
-      } else {
-        return {
-          ...state,
-          cartItems: [...state.cartItems, newProduct],
-        };
-      }
+      return {
+        ...state,
+        cartItems: inCart
+          ? state.cartItems.map((productFromCart) =>
+              productFromCart.productId === newProduct.productId
+                ? // if the product in the cart is that from action then spread it and modify the quantity for it
+                  {
+                    ...productFromCart,
+                    qty: productFromCart.qty + action.payload.qty,
+                  }
+                : // if not then return the products in the action as they were in the cart
+                  productFromCart
+            )
+          : // if the inCart is false then return the old cart and add the new product to it
+            [...state.cartItems, newProduct],
+      };
     case "ADD_TO_CART_FAIL":
       return { error: action.payload };
     case "REMOVE_FROM_CART":
